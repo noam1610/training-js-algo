5694c13182005e76a8219342b49c21a28d500f12
noam1610 <nbenlolo@yoobic.com>
2017-06-02 12:55:30 +0200

    amelioration exo2

--- exo2.js
+++ exo2.js
@@ -3,10 +3,17 @@
 // For example you should return 3 for [3, 18] because 2*2 = 4, 3*3 = 9 , 4*4 = 16
 
 function getNumberOfSquare(A, B) {
-    if (Math.floor(Math.sqrt(A)) == Math.sqrt(A)) {
-        return Math.floor(Math.sqrt(B)) - Math.floor(Math.sqrt(A)) + 1;
+
+    var A_use = A >= 0 ? A : 0;
+
+    if (B < 0) {
+        return 0;
+    } else if (A_use === B && Math.floor(Math.sqrt(B)) === Math.sqrt(B)) {
+        return 1;
+    } else if (Math.floor(Math.sqrt(A_use)) === Math.sqrt(A_use)) {
+        return Math.floor(Math.sqrt(B)) - Math.floor(Math.sqrt(A_use)) + 1;
     } else {
-        return Math.floor(Math.sqrt(B)) - Math.floor(Math.sqrt(A));
+        return Math.floor(Math.sqrt(B)) - Math.floor(Math.sqrt(A_use));
     }
 }
 
@@ -16,9 +23,20 @@ var listTest = [
     [4, 16],
     [20, 23],
     [1, 10],
-    [2, 34]
+    [2, 34],
+    [7, 7],
+    [7, 8],
+    [4, 4],
+    [8, 9],
+    [-8, 9],
+    [-1, 10],
+    [-2, -34],
+    [-4, -4],
+    [-3, 2],
+    [-5, -5],
+
 ]
 
-for (var i = 0; i < listTest.length; i++) {
-    console.log(listTest[i][0] + ' - ' + listTest[i][1] + ' --> ' + getNumberOfSquare(listTest[i][0], listTest[i][1]))
-}
+listTest.forEach(function(item) {
+    console.log(item[0] + ' - ' + item[1] + ' --> ' + getNumberOfSquare(item[0], item[1]))
+})
