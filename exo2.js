9e2505223330dd3c7d7f0b8ca48a5a726bbaf92a
noam1610 <nbenlolo@yoobic.com>
2017-06-11 21:35:03 +0200

    Exo 2: auto testing function

--- exo2.js
+++ exo2.js
@@ -4,28 +4,34 @@
 
 function getNumberOfSquare(A, B) {
 
-    var A_use = Math.max(0, A);
+    var A = Math.max(0, A);
 
     if (B < A) {
         return 0;
     } else if (B < 0) {
         return 0;
-    } else if (A_use === B && Math.floor(Math.sqrt(B)) === Math.sqrt(B)) {
+    } else if (A === B && Math.floor(Math.sqrt(B)) === Math.sqrt(B)) {
         return 1;
-    } else if (Math.floor(Math.sqrt(A_use)) === Math.sqrt(A_use)) {
-        return Math.floor(Math.sqrt(B)) - Math.floor(Math.sqrt(A_use)) + 1;
+    } else if (Math.floor(Math.sqrt(A)) === Math.sqrt(A)) {
+        return Math.floor(Math.sqrt(B)) - Math.floor(Math.sqrt(A)) + 1;
     } else {
-        return Math.floor(Math.sqrt(B)) - Math.floor(Math.sqrt(A_use));
+        return Math.floor(Math.sqrt(B)) - Math.floor(Math.sqrt(A));
     }
 }
 
-var listTest = []
-for (var i = 0; i < 10; i++) {
-    var value1 = Math.floor(Math.random(0, 1) * 100 - 50);
-    var value2 = Math.floor(Math.random(0, 1) * 100 - 50);
-    listTest.push([value1, value2])
+
+
+var generateArrayOfTestingInput = function(min, max, size) {
+    var listTest = []
+    for (var i = 0; i < size; i++) {
+        var value1 = Math.floor(Math.random(0, 1) * (max - min) + min);
+        var value2 = Math.floor(Math.random(0, 1) * (max - value1) + value1);
+        listTest.push([value1, value2])
+    }
+    return listTest;
 }
 
-listTest.forEach(function(item) {
+var testList = generateArrayOfTestingInput(-50, 50, 100);
+testList.forEach(function(item) {
     console.log(item[0] + ' / ' + item[1] + ' --> ' + getNumberOfSquare(item[0], item[1]))
 })
