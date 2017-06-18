aa26bd6d54fca0c37424fba77deb708f69c74c40
noam1610 <nbenlolo@yoobic.com>
2017-06-18 16:58:10 +0200

    Exo 4 Working

--- exo4_ES6.js
+++ exo4_ES6.js
@@ -10,14 +10,13 @@ const getMinimalValueInArray = (array) => {
     if (array.length === 0) {
         return MAX;
     } else {
-        console.log('array.length', array.length)
         let min = MAX;
         array.forEach((subArray, index) => {
             if (Number.isInteger(subArray)) {
                 if (subArray < min) {
                     min = subArray;
                 }
-            } else if (subArray.length > 1) {
+            } else if (subArray.length > 0) {
                 if (getMinimalValueInArray(subArray) < min) {
                     min = getMinimalValueInArray(subArray);
                 }
@@ -30,7 +29,9 @@ const getMinimalValueInArray = (array) => {
 
 
 const arr1 = [1, 2, 5, 3];
-const arr2 = [12, 22, [4, 5, 7], 38, [8,[[[[[3]]]]]]];
-
+const arr2 = [12, 22, [4, 5, 7], 38, [8, 2]];
+const arr3 = [3, [[1]]],
+   
 console.log(arr1 + ' --- --- - >' + getMinimalValueInArray(arr1));
 console.log(arr2 + ' --- --- - >' + getMinimalValueInArray(arr2));
+console.log(arr3 + ' --- --- - >' + getMinimalValueInArray(arr3));
